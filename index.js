8e414d512155ae7248d4ce544b3a74ded4c7950c
Kartikay <kartikay_2101ce32@iitp.ac.in>
2023-11-07 19:14:04 +0530

    Added .env
    Signed-off-by: Kartikay <kartikay_2101ce32@iitp.ac.in>

--- index.js
+++ index.js
@@ -1,78 +1,69 @@
 var nodemailer = require('nodemailer');
 const express = require('express');
 const app = express();
-const port = 3001;
-
-
-
+const dotenv = require('dotenv');
+dotenv.config();
 var cron = require('node-cron');
 
 const fs = require('fs');
 const filePath = './content.txt';
 const textData = fs.readFileSync(filePath, 'utf8')
-
+console.log(process.env.USER)
+console.log(process.env.PASS)
+console.log(process.env.GSHEET_APPS_SCRIPT)
 var transporter = nodemailer.createTransport({
   service: 'gmail',
   secure: true,
   port: 465,
   auth: {
-    user: 'kartikaysaxena12@gmail.com',
-    pass: 'stdswtkaasyoonpq'
+    user: process.env.USER,
+    pass: process.env.PASS
   }
 });
-var mailOptions = {
-    from: 'kartikaysaxena12@gmail.com',
-    to: 'kartikay_2101ce32@iitp.ac.in',
-    subject: 'Requesting for Internship',
-    text: fs.readFileSync(filePath, 'utf8'),
-    attachments : [
-        {
+async function main() {
+  try {
+    const data = await fetch(process.env.GSHEET_APPS_SCRIPT);
+    const json = await data.json();
+    console.log(json);
+
+    const mailPromises = json.map(async item => {
+      const mailOptions = {
+        from: process.env.USER,
+        to: item.email,
+        subject: 'Requesting for Internship',
+        text: textData,
+        attachments: [
+          {
             filename: 'Resume_Kartikay.pdf',
             path: './Resume_Kartikay.pdf'
-        }
-    ]
-    
-};
+          }
+        ]
+      };
 
-async function main() {
-    var data = await fetch('https://script.google.com/macros/s/AKfycbyO3S2CWxqsaFgzD8313-dksFBCQ_cbx48kiIey1t_NtsbMzm0gCNBlQNUzTn0v3DR8/exec');
-    var json = await data.json();
-    console.log(json)
-    json.forEach(item => {
-        
+      mailOptions.text = textData;
+      console.log(item.email);
+      console.log(item.isMale);
 
-        mailOptions.text = textData
-        mailOptions.to = item.email;
-        console.log(item.email)
-        console.log(item.isMale)
-        if (item.isMale===0) {
-            mailOptions.text = "Dear Ma'am" + mailOptions.text.split('$')[0] + item.company + mailOptions.text.split('$')[1]; 
-        } else if(item.isMale===1) {
-            mailOptions.text = "Dear Sir" + mailOptions.text.split('$')[0] + item.company + mailOptions.text.split('$')[1]; 
-        }
-        transporter.sendMail(mailOptions,async function(error, info){
-            if (error) {
-              console.log(error);
-            } else {
-              var sentMail = {}
-              sentMail.company = item.company
-              sentMail.email = item.email
-              console.log('Email sent: ' + info.response);
-              try {
-                const updateSentEmails = await fetch("https://script.google.com/macros/s/AKfycbz3eR18bmbL47z-7cEhQI5eSgSepOkGJ-yauDtqsxbuVlqRaYk4LVDrbr1gwivXj-JcpA/exec",{
-                    method: 'POST',
-                    body: JSON.stringify(sentMail)
-                  })
-                  const responseData = await updateSentEmails.text();
-                  console.log(responseData);
-              }
-              catch {
-                console.log('err')
-              }
-            }
-          });
-    })
+      if (item.isMale === 0) {
+        mailOptions.text = `Dear Ma'am ${textData.split('$')[0] + item.company + textData.split('$')[1]}`;
+      } else if (item.isMale === 1) {
+        mailOptions.text = `Dear Sir ${textData.split('$')[0] + item.company + textData.split('$')[1]}`;
+      }
+
+      try {
+        const info = await transporter.sendMail(mailOptions);
+        console.log('Email sent:', info.response);
+      } catch (error) {
+        console.error('Error sending email:', error);
+      }
+    });
+
+    await Promise.all(mailPromises);
+
+  
+  } catch (error) {
+    console.error('Error:', error);
+  }
 }
-main();
 
-module.exports = app;
+main();
